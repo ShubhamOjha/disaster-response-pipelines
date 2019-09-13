93eeff0fe289ac67b5f65b89025686b06420703f
Shubham Ojha <shubh1795@gmail.com>
2019-09-13 18:45:12 +0000

    Model Training and saving

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,100 @@
+# import libraries
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
+import re
+import nltk
+import pickle
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.model_selection import train_test_split
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.base import BaseEstimator, TransformerMixin
 
+print(nltk.download(['punkt', 'stopwords', 'wordnet']))
+print(nltk.download('averaged_perceptron_tagger'))
+
+print('test')
 
 def load_data(database_filepath):
-    pass
+    # load data from database
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql_table(table_name='DisasterResponse', con=engine)
+    X = df['message']
+    Y = df.iloc[:,4:]
+    category_names = list(df.columns[4:])
+    
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+    tokens = word_tokenize(re.sub(r"[^a-zA-Z0-9]",' ', text).lower())
+    tokens = [token for token in tokens if tokens not in stopwords.words('english')]
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+    for token in tokens:
+        clean_token = lemmatizer.lemmatize(token)
+        clean_tokens.append(clean_token)
+    return clean_tokens
 
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+
+    def starting_verb(self, text):
+
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            if len(pos_tags) > 0:
+                first_word, first_tag = pos_tags[0]
+                if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                    return True
+        return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+                        ('vect', CountVectorizer(tokenizer=tokenize)),
+                        ('tfidf', TfidfTransformer()),
+                        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+                        ])
+
+    parameters = {'clf__estimator__n_estimators': [50, 100],
+                  'clf__estimator__min_samples_split': [2, 3, 4],
+                  'clf__estimator__criterion': ['entropy', 'gini']
+                 }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = model.predict(X_test)
+    y_pred_df = pd.DataFrame(y_pred, columns = category_names)
+    for column in category_names:
+        print('------------------------------------------------------\n')
+        print('FEATURE : {}\n'.format(column))
+        print(classification_report(Y_test[column],y_pred_df[column]))
 
 
 def save_model(model, model_filepath):
-    pass
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
