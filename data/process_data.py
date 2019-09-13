ec930690e811ea640c7db7f75cdb2449d04f7a7d
Shubham Ojha <shubh1795@gmail.com>
2019-09-13 11:48:40 +0000

    Loading, Processing and Storing Data in DB

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,54 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages, categories, on='id')
+    return df
 
 def clean_data(df):
-    pass
+    import pdb; pdb.set_trace()
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand=True)
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+    
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.apply(lambda x : x[:-2])
+    
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1:]
+    
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(str)
+    # drop the original categories column from `df`
+    df.drop(['categories'], axis = 1, inplace = True)
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+    # check number of duplicates
+    print("Number of Duplicated before duplication removal {} out of {}".format(df.duplicated().sum(), df.shape[0]))
+    
+    # drop duplicates
+    df = df.drop_duplicates()
+    df.drop(['original'], axis =1, inplace = True)
+    
+    # check number of duplicates
+    print("Number of Duplicated after duplication removal {} out of {}".format(df.duplicated().sum(), df.shape[0]))
+    
+    return df
+    
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    df.to_sql('DisasterResponse', engine, index=False)  
 
 
 def main():
